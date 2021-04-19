699440ea5bf112cce7dee0b3bc38f64bda801689
HoangQuan <quandhcn@gmail.com>
2021-04-19 14:01:00 +0700

    Add memories game

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,15 +124,58 @@
     
     //===== 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    var imgButterfly = $('.large.shiny.butterfly');
+    var pos = document.getElementById('profile-button').getBoundingClientRect();
+    var timelineFlutterFly = new TimelineMax({yoyo: true, repeat: -1}) 
+      .to(imgButterfly,  1, { scaleX:0.5, ease: Back.easeInOut.config(1.7)})
+      .to(imgButterfly, .3, { scaleX:1, ease: Back.easeInOut.config(1.7)}).delay(3);
+     
+
+    var img = $('.butterfly');
+    var offset = img.offset();
+    var center_x = (offset.left) + (51/2);
+    var center_y = (offset.top) + (51/2);
+    var mouse_x = window.innerWidth - 200;
+    var mouse_y = window.innerHeight - 200; //evt.pageY;
+
+    var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+    var degree = (radians * (180 / Math.PI) * 1); 
+    var timelineMoveButterfly = new TimelineMax({
+      yoyo: true
+    })
+      .to(imgButterfly,  1, { rotation: degree })
+      .to(imgButterfly,  5, { left: mouse_x, top: mouse_y, ease: Power1.easeInOut})
+      .to(imgButterfly,  1, { rotation: 0 })
+      .to(imgButterfly,  1, { rotation: -31, ease: Power1.easeInOut})
+      .to(imgButterfly,  5, { rotation: -34, ease: Power1.easeInOut})
+      .to(imgButterfly,  1, { rotation: (180 / Math.PI) - degree })
+      .to(imgButterfly,  5, { left:211, top: -18, ease: Power1.easeInOut})
+      .to(imgButterfly,  1, { rotation: -31, ease: Power1.easeInOut}).delay(3);
+
+    setInterval(function() {
+      console.log(timelineMoveButterfly.totalProgress());
+      if(timelineMoveButterfly.progress() == 1){
+        timelineMoveButterfly.restart();
+      }
+    }, 5000)
+
+    // $( "body" ).mousemove(function( event ) {
+    //   var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+    //   var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+    //   console.log(clientCoords);
+    //     var center_x = (offset.left) + (51/2);
+    //     var center_y = (offset.top) + (51/2);
+
+    //     var mouse_x = event.pageX;
+    //     var mouse_y = event.pageY; //evt.pageY;
+
+    //     var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+    //     var degree = (radians * (180 / Math.PI) * 1); 
+
+    //     timelineMoveButterfly.to(imgButterfly,  .0, { rotation: degree })
+    //       .to(imgButterfly,  .001, { left: mouse_x, top: mouse_y, ease: Back.easeInOut.config(1.7)});
+
+    // });
+
     
 }(jQuery));
